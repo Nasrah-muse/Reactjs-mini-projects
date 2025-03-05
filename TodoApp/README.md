40f0b3ddcc6e8cd7690ccea9815add9377f17cbb
Nasrah-muse <nazronafta143@gmail.com>
2025-03-05 20:15:45 +0300

    feat: add complete todolist app

---
+++ TodoApp/README.md
@@ -0,0 +1,32 @@
+# To-Do List App
+
+A simple and interactive to-do list application built with React.
+ This app allows users to add tasks, mark them as completed, and delete them.
+
+![Project Preview](./src/assets/Screenshot%202025-03-05%20200059.png)
+
+## Features
+
+- **Add Tasks**: Enter a new task in the input field and click "Add Task" to add it to the list.
+- **Mark as Completed**: Click the checkbox or the task text to toggle its completion status.
+- **Delete Tasks**: Click the "Delete" button to remove a task from the list.
+- **Responsive Design**: The app is styled with Tailwind CSS for a clean and modern look.
+
+## How to Use
+
+1. **Add a Task**:
+   - Type your task in the input field.
+   - Click the "Add Task" button to add it to the list.
+
+2. **Mark a Task as Completed**:
+   - Click the checkbox next to the task or click the task text itself to toggle its completion status.
+   - Completed tasks will be displayed with a line-through and grayed-out text.
+
+3. **Delete a Task**:
+   - Click the "Delete" button next to the task you want to remove.
+
+## Technologies Used
+
+- **React**: A JavaScript library for building user interfaces.
+- **Tailwind CSS**: A utility-first CSS framework for styling the app.
+
---
+++ TodoApp/src/App.jsx
@@ -0,0 +1,70 @@
+import { useState } from 'react'
+ 
+function App() {
+  const [task, settask] = useState('')
+  const [todos, setTodos] = useState([])
+  
+  const handleAdd = ()=>{
+    if(task.trim() !== ''){
+      const newTask = {
+        id : Date.now(),
+        text: task,
+        completed: false
+      }
+      setTodos([...todos, newTask])
+      settask("")
+    }
+  }
+  const toggleComplete = (id)=>{
+    setTodos(
+      todos.map((todo)=> todo.id === id? {...todo, completed: !todo.completed} : todo)
+    )
+  }
+  const removeTask =  (id)=>{
+    setTodos(todos.filter((todo)=> todo.id !== id ))
+  }
+  return (
+    <div className='min-h-screen bg-indigo-900 flex items-center justify-center'>
+      <div className='bg-white  p-6 shadow-md rounded-xl '>
+       <div className='space-x-4'>
+       <input type="text"
+        placeholder='Enter New task'
+        value={task}
+        onChange={(e)=> settask(e.target.value)}
+        className='border-2 border-gray-200 p-2 rounded  focus:border-cyan-500 outline-0'
+      
+         />
+         <button
+         className=' bg-cyan-600 py-2  px-3 rounded text-white font-bold cursor-pointer hover:bg-cyan-800'
+          onClick={handleAdd}>Add Task</button>
+       </div>
+
+       <div className='mt-4 bg-gray-100 shadow-xl rounded '>
+       {
+          todos.map((todo)=>(
+            <li key={todo.id}
+            className='flex justify-between p-1 items-center'
+            >
+              <input type="checkbox" checked= {todo.completed} 
+              onChange={()=> toggleComplete(todo.id)}
+               />
+              <span 
+              onClick={()=> toggleComplete(todo.id)}
+              className={`${todo.completed? 'line-through text-gray-400': 'text-indigo-800 font-bold'} ` }
+              >{todo.text}</span>
+              <button
+              className='bg-rose-500 py-1 px-2 text-white font-bold rounded hover:bg-rose-600 cursor-pointer'
+               onClick={()=> removeTask(todo.id)}>Delete</button>
+
+            </li>
+          ))
+         }
+       </div>
+ 
+      </div>
+ 
+    </div>
+  )
+}
+
+export default App
